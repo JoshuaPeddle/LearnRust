10fdaa63e95b9599ebf219af1b2523738d94ee4e
JoshuaPeddle <joshuapeddle128@gmail.com>
2022-07-30 19:24:56 -0230

    Rename and cleanup

---
+++ webserver1/src/main.rs
@@ -0,0 +1,29 @@
+use std::io;
+use std::net::TcpListener;
+
+use webserver1;
+
+extern crate chrono;
+
+
+fn main() -> io::Result<()> {
+    println!("Starting server...");
+
+    let listener = TcpListener::bind("127.0.0.1:8001")?;
+
+    println!("Server started!");
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                match webserver1::handle_client(stream) {
+                    Err(e) => eprintln!("Error handling client: {}", e),
+                    _ => (),
+                }
+            },
+            Err(e) => eprintln!("Connection failed: {}", e),
+        }
+    }
+
+    Ok(())
+}
